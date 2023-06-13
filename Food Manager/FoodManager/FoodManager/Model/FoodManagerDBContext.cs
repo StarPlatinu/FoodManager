bc9c6461462282dc41a7086b3d8af645853cd72c
StarPlatinu <thanhvp1509@gmail.com>
2023-06-14 01:39:15 +0700

    make an clearly base project

---
+++ Food Manager/FoodManager/FoodManager/Model/FoodManagerDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FoodManager.Model
+{
+    public class FoodManagerDBContext : DbContext
+    {
+        public FoodManagerDBContext(DbContextOptions<FoodManagerDBContext> options) : base(options) 
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
