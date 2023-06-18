fc174799fd1bb052b2345870b3024a24afa63057
StarPlatinu <thanhvp1509@gmail.com>
2023-06-18 23:40:50 +0700

    CategoryRepository

---
+++ Food Manager/FoodManager/FoodManager/Repository/CategoryRepository.cs
@@ -0,0 +1,25 @@
+﻿using FoodManager.Model;
+using FoodManager.Repository.IRepository;
+
+namespace FoodManager.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly FoodManagerDBContext _db;
+        public CategoryRepository(FoodManagerDBContext db) : base(db)
+        {
+            _db = db;
+        }
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Update(Category category)
+        {
+           var objFromDb = _db.Categories.FirstOrDefault(u=>u.Id == category.Id);
+            objFromDb.Name = category.Name;
+            objFromDb.DisplayOrder = category.DisplayOrder;
+        }
+    }
+}
---
+++ Food Manager/FoodManager/FoodManager/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,10 @@
+﻿using FoodManager.Model;
+
+namespace FoodManager.Repository.IRepository
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+        void Update(Category category);
+        void Save();
+    }
+}
