5123fddd542bf09ae4e8fffd2304e38dffb9ea33
Quang <quanghm@kis-v.com.vn>
2023-07-21 00:06:54 +0700

    Add search

--- Food Manager/FoodManager/FoodManager/Pages/Customer/Home/Index.cshtml.cs
+++ Food Manager/FoodManager/FoodManager/Pages/Customer/Home/Index.cshtml.cs
@@ -1,4 +1,5 @@
 using FoodManager.Model;
+using FoodManager.Repository;
 using FoodManager.Repository.IRepository;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -8,8 +9,9 @@ namespace FoodManager.Pages.Customer.Home
     public class IndexModel : PageModel
     {
 		private readonly IUnitOfWork _unitOfWork;
-
-		public IndexModel(IUnitOfWork unitOfWork)
+        [BindProperty]
+        public string SearchQuery { get; set; }
+        public IndexModel(IUnitOfWork unitOfWork)
 		{
 			_unitOfWork = unitOfWork;
 		}
@@ -22,5 +24,21 @@ namespace FoodManager.Pages.Customer.Home
 			MenuItemList = _unitOfWork.MenuItem.GetAll(includeProperties: "Category,FoodType");
 			CategoryList = _unitOfWork.Category.GetAll(orderby: u => u.OrderBy(c => c.DisplayOrder));
 		}
+
+		public IActionResult OnPost(string search)
+		{
+
+            MenuItemList = _unitOfWork.MenuItem.GetAll(includeProperties: "Category,FoodType");
+            CategoryList = _unitOfWork.Category.GetAll(orderby: u => u.OrderBy(c => c.DisplayOrder));
+
+            if (!string.IsNullOrEmpty(SearchQuery))
+            {
+                return Page();
+            }
+            else
+            {
+                return RedirectToPage("Error");
+            }
+        }
 	}
 }
